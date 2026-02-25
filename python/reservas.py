e01f01d64843454f127a7eda24e48486a53a68a8
Nicoleealfaroo <nikitaalfaro1002@gmail.com>
2026-02-24 19:22:26 -0600

    Update reservas.py

--- python/reservas.py
+++ python/reservas.py
@@ -1,1 +1,22 @@
+def main():
+    reservas = 5
+    capacidad = [ ]
 
+def hay_cupo(reservas, capacidad):
+    if 0 < capacidad <=5:
+        Salida: True
+
+def mostrar_resumen(reservas, capacidad):
+    print(f"Cupos disponibles: {len(reservas)}")
+    for reserva in reservas:
+        print(reserva)
+
+    while True:
+        nombre = input("Ingrese su nombre: ")
+        if hay_cupo(reservas, capacidad):
+            reservas.append(nombre)
+    
+    mostrar_resumen(reservas, capacidad)
+
+if __name__ == "__main__":
+    main()
